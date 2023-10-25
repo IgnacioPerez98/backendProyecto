6afcf88c033dcd74d7996fdfd8a5384ea0fb8248
IgnacioPerez98 <85960357+IgnacioPerez98@users.noreply.github.com>
2023-10-25 20:32:15 -0300

    nacho

--- servicios/SHA512.js
+++ servicios/SHA512.js
@@ -3,8 +3,6 @@ let ServicioEncriptacion = {
     cifrar: function (text){
         return crypto.createHash('sha512').update(text).digest('hex');
     }
-
-
 }
 
 module.exports = ServicioEncriptacion;
