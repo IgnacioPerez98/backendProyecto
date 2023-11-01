19e297cf844b53608d19a0cf391ce2d4729a3969
IgnacioPerez98 <85960357+IgnacioPerez98@users.noreply.github.com>
2023-11-01 20:34:37 -0300

    cors & deploy config

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 const swaggerSpec = require('./routes/swagger'); // Importa la configuración de Swagger
 
 //Endpoints
@@ -11,6 +12,27 @@ const votacionRoute = require('./endpoints/votacion');
 //Almaceno instancia de express
 const app = express();
 app.use(express.json());
+//Configuro el cors
+let allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:4200',
+    'https://www.desarrollowebback.duckdns.org',
+    'http://www.desarrollowebback.duckdns.org'
+
+];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // Middleware para servir la documentación Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
